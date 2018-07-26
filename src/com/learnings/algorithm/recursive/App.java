26ca38bc27028d8bf25b4794cd2588e64eb81b6e
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-07-25 19:16:09 -0700

    Binary Tree Implementation with Recursive - Madanraj Venkatesan

--- src/com/learnings/algorithm/recursive/App.java
+++ src/com/learnings/algorithm/recursive/App.java
@@ -8,6 +8,6 @@ public class App {
 		System.out.println("Index: " + new LinearSearch().searchUsingRecursion(new Object[]{"First", "Second", "Third"}, "First"));
 		
 		//Recursive Binary Search
-		System.out.println("Index: " + new BinarySearch().searchUsingRecursion(new int[] {1, 3, 5, 7, 18, 22, 234, 567, 677, 766, 877, 898, 999, 1000}, 0));
+		System.out.println("Index: " + new BinarySearch().searchUsingRecursion(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, 11));
 	}
 }
