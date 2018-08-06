5f77e239df2b771a30439462583bac9f4c56db04
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-08-06 13:34:00 -0700

    Insertion Sort Modification - Madanraj Venkatesan

--- src/com/learnings/algorithm/insertion_sort/InsertionSort.java
+++ src/com/learnings/algorithm/insertion_sort/InsertionSort.java
@@ -4,8 +4,8 @@ public class InsertionSort {
 	
 	public int [] sort(int [] inputArray) {
 		
-		for(int k = 0; k < (inputArray.length - 1); k++) {
-			for(int i = k, j = i + 1; i >= 0; i--) {
+		for(int k = 0; k < inputArray.length; k++) {
+			for(int i = k, j = i + 1; i >= 0 && j<inputArray.length; i--) {
 				if(inputArray[i] > inputArray[j]) {
 					swapElements(inputArray, i, j);
 					j--;
