cb1fd470203f0e0df935e131c1992d2603731913
Madanraj Venkatesan <madanraj.venkatesan@gmail.com>
2018-08-06 20:44:59 -0700

    Merge Sort Implementation - Madanraj Venkatesan

---
+++ src/com/learnings/algorithm/merge_sort/App.java
@@ -0,0 +1,10 @@
+package com.learnings.algorithm.merge_sort;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		MergeSort mergeSort = new MergeSort();
+		mergeSort.sort(new int[] {8, 6, 1, 5, 9, 2, 2, 7, 0, 9, 0, 5, 1});
+	}
+}
