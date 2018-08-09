65844f06890524373090fcf720c5874019840dcf
bn514d <bn514d@A5537442.nw.nos.boeing.com>
2018-08-09 13:26:58 -0700

    Quick Sort Implementation - Madanraj Venkatesan

--- src/com/learnings/algorithm/quick_sort/App.java
+++ src/com/learnings/algorithm/quick_sort/App.java
@@ -4,14 +4,13 @@ public class App {
 
 	public static void main(String[] args) {
 
-		int [] inputArray = new int[] {8, 6, 1, 5, 9, 2, 2, 7, 0, 9, 0, 5, 1, 7};
+		int [] inputArray = new int[] {8, 6, 1, 5, 9, 2, 2, 7, 0, 9, 0, 5, 1, 7, 9, 11, 1024, 8};
 		QuickSort quickSort = new QuickSort();
 
 		System.out.print("Input Array: [");
 		for(int i=0; i<inputArray.length; i++)
 			System.out.print(inputArray[i] + " ");
-		System.out.print("]\n");
-		System.out.println();
+		System.out.print("]\n\n");
 
 		quickSort.sort(inputArray);
 	}
